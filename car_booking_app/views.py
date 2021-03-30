cedb9fd6c1f2a6b7915d406f5db798dde9634967
unknown <dubeyshreyashi143@gmail.com>
2021-03-30 14:38:15 +0530

    adding file after migration

--- car_booking_app/views.py
+++ car_booking_app/views.py
@@ -19,31 +19,33 @@ def index(request):
 def register(request):
 
     registered = False
-
-    # If it's a HTTP POST, we're interested in processing form data.
     if request.method == 'POST':
         user_form = UserForm(data=request.POST)
         profile_form = UserProfileForm(data=request.POST)
 
         if user_form.is_valid() and profile_form.is_valid():
             user = user_form.save()
+
             user.set_password(user.password)
             user.save()
 
             profile = profile_form.save(commit=False)
             profile.user = user
+
             profile.save()
+
             registered = True
+
         else:
             print user_form.errors, profile_form.errors
+
     else:
         user_form = UserForm()
         profile_form = UserProfileForm()
 
-    # Render the template depending on the context.
     return render(request,
             'car_booking_app/register.html',
-            {'user_form': user_form, 'profile_form': profile_form, 'registered': registered} )
+            {'user_form': user_form, 'profile_form': profile_form, 'registered': registered})
 
 
 def user_login(request):
